e5b54022db8e3525a3e6a747ebf0059ee0cfd096
Narenzy <mahankotaras@gmail.com>
2026-06-23 09:07:02 +0200

    deleted thml files

---
+++ src/js/api.js
@@ -0,0 +1,7 @@
+const gallery = document.querySelector('gallery');
+
+const base_url = 'https://jsonplaceholder.typicode.com';
+const END_POINT = '/users';
+const url = base_url + END_POINT;
+
+fetch(url);
